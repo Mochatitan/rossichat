281f42fbda32fefd221180cccdb57c61e0c1fc37
Mochatitan <mochatitanstudios@gmail.com>
2025-08-15 09:48:33 -0400

    updated port

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,5 @@ app.get("*", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 80;
+server.listen(PORT, "0.0.0.0");
